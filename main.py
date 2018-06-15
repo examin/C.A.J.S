228ca96a8f887d8439ffc4189ff5818b28b7e503
manish sharma <manish.sharmaserver008@yahoo.com>
2018-06-15 07:32:17 +0530

    Optimisation and Added tomcatNative parser
    added main.py for running all spiders at once,
    Optimised parsers

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+os.system("scrapy crawl Tomcat_7")
+os.system("scrapy crawl Tomcat_8")
+os.system("scrapy crawl Tomcat_9")
+os.system("scrapy crawl TomcatNative")
+os.system("scrapy crawl OpenSSL")
+os.system("scrapy crawl Httpd")
+os.system("scrapy crawl Curl")
+os.system("scrapy crawl Connectors")
