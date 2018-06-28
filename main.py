c14286cb743236eaf8bfff41f772d589039080f5
manish sharma <manish.sharmaserver008@yahoo.com>
2018-06-28 16:47:27 +0530

    resolved issue #5 Started process with a shell
    Now used crawler process

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-import os
-os.system("scrapy crawl Tomcat_7")
-os.system("scrapy crawl Tomcat_8")
-os.system("scrapy crawl Tomcat_9")
-os.system("scrapy crawl TomcatNative")
-os.system("scrapy crawl OpenSSL")
-os.system("scrapy crawl Httpd")
-os.system("scrapy crawl Curl")
-os.system("scrapy crawl Connectors")
+import logging
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+
+
+
+if __name__ == '__main__':
+    configure_logging(install_root_handler=False)
+    logging.basicConfig(
+        filename='scraper.log',
+        format='%(created) - f%(levelname)s: %(message)s',
+        level=logging.INFO
+    )
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('Tomcat_7')
+    process.crawl('Tomcat_8')
+    process.crawl('Tomcat_9')
+    process.crawl('TomcatNative')
+    process.crawl('OpenSSL')
+    process.crawl('Httpd')
+    process.crawl('Curl')
+    process.crawl('Connectors')
+
+    process.start()
